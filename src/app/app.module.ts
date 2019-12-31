6d2aa28ba922015d2072989ff2f2ada2c3bec7b2
Manikanta Reddy <madornal@microsoft.com>
2019-12-31 01:15:53 -0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
+import { MsalModule } from '@azure/msal-angular';
+
+import 'bootstrap';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+
+import AuthSettings from './settings/auth.json';
+
+console.log(AuthSettings);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MsalModule.forRoot(AuthSettings)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
